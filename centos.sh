8d1af7acdc9ca84d8bff1dec8ba32dc0755adf53
baaiim <baaiimcool@gmail.com>
2015-03-14 16:45:44 +0800

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
-OPT=$1
-case $OPT in
-  -c5|-C5) 
-  	wget http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-    sudo rpm -Uvh epel-release-5*.rpm
-  	;;
-  -c6|-C6) 
-  	wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    sudo rpm -Uvh epel-release-6*.rpm
-  	;;
-  -c7|-C7) 
-  	wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-    sudo rpm -Uvh epel-release-7*.rpm 
-  	;;
-   *) 
-    echo "Bad argument!" 
-    echo "	-c5 for centos 5 "
-    echo "	-c6 for centos 6 "
-    echo "	-c7 for centos 7 "
-    exit 0	
-    ;;
-esac
 cd 
 mkdir horizon
 cd horizon
+yum install epel-release
 yum install jq -y
 yum install python -y
 yum install java-1.7.0-openjdk.x86_64 -y
 yum install unzip -y
 yum install screen -y
 yum install curl -y
-wget http://files.nhzcrypto.org/binaries/hz_v3.8.zip
+wget http://downloads.horizonplatform.io/binaries/hz_v3.8.zip
 unzip hz*.zip 
 cd hz
 IP=$(wget -qO- ifconfig.me/ip)
@@ -74,7 +53,7 @@ echo "DONE"
 sleep 2
 
 TGL=$(date +%Y-%m-%d)
-CODE=$(curl -d requestType="markHost" -d host="$IP" -d weight="100" -d date="$TGL" -d secretPhrase="$2" http://127.0.0.1:7776/nhz | jq -r '.hallmark')
+CODE=$(curl -d requestType="markHost" -d host="$IP" -d weight="100" -d date="$TGL" -d secretPhrase="$1" http://127.0.0.1:7776/nhz | jq -r '.hallmark')
 sed -i s/nhz.myHallmark=/nhz.myHallmark=$CODE/g /root/horizon/hz/conf/nhz-default.properties
 sleep 2
 screen -S hallmarked -X quit
